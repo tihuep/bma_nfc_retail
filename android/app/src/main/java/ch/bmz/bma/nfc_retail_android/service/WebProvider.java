ddd37c7355128156487c55f6d89acb7e09b8c90f
Timon <timon.hueppi@gmail.com>
2021-10-26 22:05:25 +0200

    adds request capability

---
+++ android/app/src/main/java/ch/bmz/bma/nfc_retail_android/service/WebProvider.java
@@ -0,0 +1,14 @@
+package ch.bmz.bma.nfc_retail_android.service;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class WebProvider {
+    public static void doRequest(StringRequest stringRequest, Context context) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+        queue.add(stringRequest);
+    }
+}
