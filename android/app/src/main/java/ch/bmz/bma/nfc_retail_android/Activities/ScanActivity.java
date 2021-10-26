3ecd80f52f97421d53334d919610573c0a30ed10
Timon <timon.hueppi@gmail.com>
2021-10-26 15:02:58 +0200

    adds NFC capability

--- android/app/src/main/java/ch/bmz/bma/nfc_retail_android/Activities/ScanActivity.java
+++ android/app/src/main/java/ch/bmz/bma/nfc_retail_android/Activities/ScanActivity.java
@@ -3,12 +3,25 @@ package ch.bmz.bma.nfc_retail_android.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 import ch.bmz.bma.nfc_retail_android.R;
 
@@ -31,6 +44,12 @@ public class ScanActivity extends AppCompatActivity {
     String articleDesc;
     Float articlePrice;
 
+    NfcAdapter nfcAdapter;
+    PendingIntent pendingIntent;
+    IntentFilter writingTagFilters[];
+    Tag myTag;
+    Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,9 +70,25 @@ public class ScanActivity extends AppCompatActivity {
         scanError.setVisibility(View.GONE);
 
         defineButtonHandlers();
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            Toast.makeText(context, this.getResources().getString(R.string.no_nfc), Toast.LENGTH_LONG).show();
+            finish();
+        }
+        if (!nfcAdapter.isEnabled()) {
+            Toast.makeText(context, this.getResources().getString(R.string.activate_nfc), Toast.LENGTH_LONG).show();
+            finish();
+        }
+        readFromIntent(getIntent());
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), PendingIntent.FLAG_IMMUTABLE);
+        IntentFilter tagDetected = new IntentFilter((NfcAdapter.ACTION_TAG_DISCOVERED));
+        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+        writingTagFilters = new IntentFilter[]{tagDetected};
     }
 
     private void defineButtonHandlers() {
+        ScanActivity that = this;
         scanBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -100,6 +135,51 @@ public class ScanActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void readFromIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)){
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs = null;
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++){
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            }
+            buildTagViews(msgs);
+        }
+    }
+
+    private void buildTagViews(NdefMessage[] msgs){
+        if (msgs == null || msgs.length == 0) return;
+
+        byte[] payload = msgs[0].getRecords()[0].getPayload();
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+        int languageCodeLength = payload[0] & 0063;
+
+        try {
+            articleID = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+            articleAmount = 1;
+            scanArticleID.setText(articleID);
+            scanAmount.setText(articleAmount.toString());
+
+        }catch (UnsupportedEncodingException e) {
+            Log.e("UnsupportedEncoding", e.toString());
+        }
+    }
+
+    @Override
+    protected  void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        readFromIntent(intent);
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+            myTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        }
     }
 
     public void displayError(String message) {
