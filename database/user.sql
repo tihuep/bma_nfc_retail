a9ae25a7ad4a11025beab099767178f25b7887e4
Sven Jäger <sven.jaeger.2003@gmail.com>
2021-10-27 08:10:16 +0200

    - add datetime to payment-Table

--- database/user.sql
+++ database/user.sql
@@ -1,6 +1,6 @@
 DROP USER 'bma'@'localhost';
 
-CREATE USER 'bma'@'localhost' IDENTIFIED BY 'bma';
+CREATE USER 'bma'@'localhost' IDENTIFIED BY '(Bma2021)';
 GRANT SELECT ON *.* TO 'bma'@'localhost';
 GRANT ALL PRIVILEGES ON nfc_retail.* TO 'bma'@'localhost';
 FLUSH PRIVILEGES;
