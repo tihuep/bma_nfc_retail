15d0809d0252c8be5b94a45d846b48e94ef709e1
Sven Jäger <sven.jaeger.2003@gmail.com>
2021-10-29 14:37:32 +0200

    - remove unnecessary dependencies

--- server/src/authentication/AuthenticationService.js
+++ server/src/authentication/AuthenticationService.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const ACCESS_TOKEN_SECRET = '583a4ff987d7b563e183eb0f057d2b34eaf679417cb7a22854b6be7bae884d59271f978de3e81af6df7a70145ca1aec6057c1f8642b77cdf72182c33e3d87f1b';
